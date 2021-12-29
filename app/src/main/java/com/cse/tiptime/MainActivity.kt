37c24fba04eea16f7852c489f23721745905a42e
Nicolas Machado <nico.machado@gmail.com>
2021-12-29 13:47:13 -0300

    Initial commit

---
+++ app/src/main/java/com/cse/tiptime/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.cse.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cse.tiptime.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+    }
+}
